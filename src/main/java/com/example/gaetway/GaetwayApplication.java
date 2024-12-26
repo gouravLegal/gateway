ec2ca3a9841b1b1dbdfc8d3549ba74be5fa5190b
gourav <gourav.aggarwal@gmail.com>
2024-12-26 20:38:17 +0530

    Initial version

---
+++ src/main/java/com/example/gaetway/GaetwayApplication.java
@@ -0,0 +1,13 @@
+package com.example.gaetway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaetwayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GaetwayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gaetway/GaetwayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gaetway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaetwayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
